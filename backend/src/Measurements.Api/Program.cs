93bf2aac3cf0cef28ab9fdf6f15ff0d04c449237
Eugen Selbstreich <qduck@mail.ru>
2026-06-16 22:38:40 +0200

    add result handler

--- backend/src/Measurements.Api/Program.cs
+++ backend/src/Measurements.Api/Program.cs
@@ -1,4 +1,33 @@
+using System.Text.Json.Serialization;
+using Measurements.Api;
+using Microsoft.AspNetCore.Http.Features;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(x =>
+    {
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Provide detailed information if a request fails. (ref: https://www.rfc-editor.org/rfc/rfc9457.html)
+// Credits: Nick Chapsas -> https://www.youtube.com/watch?v=-TGZypSinpw
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+        var activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+        context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+    };
+});
+builder.Services.AddExceptionHandler<ResultExceptionHandler>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ backend/src/Measurements.Api/ResultExceptionHandler.cs
@@ -0,0 +1,40 @@
+using Measurements.Businesslogic.Entities;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Measurements.Api;
+
+public class ResultExceptionHandler: IExceptionHandler
+{
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public ResultExceptionHandler(IProblemDetailsService problemDetailsService)
+    {
+        _problemDetailsService = problemDetailsService;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is not ResultException resultException)
+        {
+            return true;
+        }
+
+        // TODO get StatusCodes from exception
+        var exceptionDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = resultException.Error,
+            Detail = resultException.Message,
+            Type = "Bad Request"
+        };
+
+        httpContext.Response.StatusCode = exceptionDetails.Status.Value;
+        return await _problemDetailsService.TryWriteAsync(
+            new ProblemDetailsContext
+            {
+                HttpContext = httpContext,
+                ProblemDetails = exceptionDetails
+            });
+    }
+}
